89f9789a5a0e71979f4ff83934e1db16b5e0e209
captainkirk666 <contactkirk@hotmail.com>
2026-07-22 14:05:02 +1000

    Refactor HTML output for championship leader display

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 const url = "https://api.jolpi.ca/ergast/f1/current/driverStandings.json";
 
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
+.then(response => response.json())
+.then(data => {
 
     const leader =
-      data.MRData
-          .StandingsTable
-          .StandingsLists[0]
-          .DriverStandings[0];
+    data.MRData
+        .StandingsTable
+        .StandingsLists[0]
+        .DriverStandings[0];
 
-    document.body.innerHTML += `
-      <h2>🏆 Championship Leader</h2>
+    document.body.innerHTML = `
+        <h1>🏎️ Formula One Championship</h1>
 
-      <h3>
-        ${leader.Driver.givenName}
-        ${leader.Driver.familyName}
-      </h3>
+        <h2>${leader.Driver.givenName} ${leader.Driver.familyName}</h2>
 
-      <p>Points: ${leader.points}</p>
-      <p>Wins: ${leader.wins}</p>
-
+        <p><strong>Points:</strong> ${leader.points}</p>
+        <p><strong>Wins:</strong> ${leader.wins}</p>
     `;
-
-  });
+});
