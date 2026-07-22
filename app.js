729d8f2a21c6909b483819b23aca1f4940e6ae66
captainkirk666 <contactkirk@hotmail.com>
2026-07-22 13:59:25 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,22 +3,10 @@ const url = "https://api.jolpi.ca/ergast/f1/current/driverStandings.json";
 fetch(url)
   .then(response => response.json())
   .then(data => {
+    console.log(data);
 
-    const leader =
-      data.MRData
-          .StandingsTable
-          .StandingsLists[0]
-          .DriverStandings[0];
-
-    document.body.innerHTML += `
-        <h2>Championship Leader</h2>
-
-        <p>
-        ${leader.Driver.givenName}
-        ${leader.Driver.familyName}
-        </p>
-
-        <p>${leader.points} points</p>
-    `;
-
+    document.body.innerHTML += "<h2>It works!</h2>";
+  })
+  .catch(error => {
+    console.error(error);
   });
